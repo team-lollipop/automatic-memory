23e493296b42552dcd3a88ee476497010b9b3659
Tasha R Zuniga <Tashazun@gmail.com>
2018-04-30 10:31:41 -0700

    User unit test passing

--- test/unit/User.test.js
+++ test/unit/User.test.js
@@ -5,8 +5,14 @@ const { getErrors } = require('./helpers');
 describe('User model', () => {
 
     it('good valid model', () => {
+        const input = {
+            name: 'Tasha Zuniga',
+            hash: 'Thisispassword',
 
-
+        };
+        const user = new User(input);
+        input._id = user._id;
+        assert.deepEqual(user.toJSON(), input);
     });
 
 
