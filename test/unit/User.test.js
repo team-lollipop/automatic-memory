d96cad9fbf9b5986bb77beda12eea8e2bccc4ac3
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-04-30 10:17:37 -0700

    start user test

---
+++ test/unit/User.test.js
@@ -0,0 +1,13 @@
+const { assert } = require('chai');
+const User = require('../../lib/models/User');
+const { getErrors } = require('./helpers');
+
+describe('User model', () => {
+
+    it('good valid model', () => {
+
+
+    });
+
+
+});
