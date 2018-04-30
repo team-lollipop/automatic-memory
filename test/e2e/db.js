5a996ce9438263d11078888d5f3c2eddceea9c84
Keli Hansen <keli.p.hansen@gmail.com>
2018-04-30 12:19:28 -0700

    appease travis

--- test/e2e/db.js
+++ test/e2e/db.js
@@ -1,6 +1,6 @@
 const connect = require('../../lib/util/connect');
 const mongoose = require('mongoose');
-const request = require('./request');
+// const request = require('./request');
 
 before(() => connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/bird-game-test'));
 after(() => mongoose.connection.close());
