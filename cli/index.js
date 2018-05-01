b3651f8197cc48581ef0a472fea187f3a9e51b1c
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-04-30 17:00:16 -0700

    add sign in to cli

--- cli/index.js
+++ cli/index.js
@@ -3,6 +3,7 @@ const request = require('superagent');
 const Game = require('./game');
 const emoji = require('./emoji');
 const server = 'http://localhost:3000';
+const colors = require('colors');
 
 
 console.log('Hi, welcome to Bird\'s eye view!', emoji.bird[1]);
@@ -11,13 +12,24 @@ console.log('Hi, welcome to Bird\'s eye view!', emoji.bird[1]);
 let token = '';
 
 const service = {
-    signup(userData) {
+    signUp(userData) {
         return request.post(`${server}/api/auth/signup`)
             .send(userData)
             .then(({ body }) => {
                 token = body.token;
+                body.message = `Welcome ${body.name.green}!`;
                 return body;
             });
+    },
+    signIn(userData) {
+        return request.post(`${server}/api/auth/signin`)
+            .send(userData)
+            .then(({ body }) => {
+                token = body.token;
+                body.message = `Welcome back, ${body.name.green}!`;
+                return body;
+            });
+
     }
     // getTask(userId, token) {
     //     return request.get(`${server}/api/task`)
